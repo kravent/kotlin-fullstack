9fa7a0604951e442a90b0d1ac048d9bbc90f6d3b
Adrián García <dev@agaman.me>
2020-02-12 03:16:01 +0100

    Fix returning ktor main page

--- backend/src/main/kotlin/me/agaman/kotlinfullstack/Application.kt
+++ backend/src/main/kotlin/me/agaman/kotlinfullstack/Application.kt
@@ -7,20 +7,30 @@ import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
-import io.ktor.response.respondOutputStream
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.route
 import me.agaman.kotlinfullstack.api.apiRouter
 
-private suspend fun ApplicationCall.respondReactPage() {
-    respondOutputStream(ContentType.Text.Html, HttpStatusCode.OK) {
-        javaClass.classLoader.getResourceAsStream("react.html")
-    }
-}
+private const val REACT_PAGE = """
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kotlin fullstack</title>
+</head>
+<body>
+<div id="root"></div>
+<script src="static/app.js"></script>
+</body>
+</html>
+"""
+
+private suspend fun ApplicationCall.respondReactPage() =
+    respondText(REACT_PAGE, ContentType.Text.Html)
 
 fun Application.module() {
     install(DefaultHeaders)
