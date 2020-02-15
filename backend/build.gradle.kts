bba330a0590aa74b473a818aeb9e405cd5904287
Adrián García <dev@agaman.me>
2020-02-15 21:31:39 +0100

    Add server logger

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation(project(":common"))
 }
 
