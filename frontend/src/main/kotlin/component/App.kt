0d027ed7847dbd41d8a1ddc5ec1d8f307a8a9d25
Adrián García <dev@agaman.me>
2020-03-08 23:21:28 +0100

    Remove some not needed things

--- frontend/src/main/kotlin/component/App.kt
+++ frontend/src/main/kotlin/component/App.kt
@@ -1,20 +1,15 @@
 package component
 
 import com.ccfraser.muirwik.components.mThemeProvider
-import com.ccfraser.muirwik.components.styles.createMuiTheme
 import component.login.loginInterceptor
 import component.store.storeProvider
 import react.RBuilder
 import react.ReactElement
 import react.router.dom.browserRouter
 
-val createMuiTheme = createMuiTheme().apply {
-    palette.type = "light"
-}
-
 fun RBuilder.app(): ReactElement {
     return storeProvider {
-        mThemeProvider(createMuiTheme) {
+        mThemeProvider {
             loginInterceptor {
                 browserRouter {
                     appRouter()
